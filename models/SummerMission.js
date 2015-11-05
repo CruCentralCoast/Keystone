afd7d65cbcbc06daccf91aafe14fa38359f89901
Andrew Kennedy <andrewk36@gmail.com>
2015-11-05 00:16:07 -0800

    added cost and leaders to summer missions

--- models/SummerMission.js
+++ models/SummerMission.js
@@ -18,7 +18,9 @@ SummerMission.add({
   url: { type: Types.Url },
   location: { type: Types.Location, initial: true, required: true, defaults: { country: 'USA' } },
 	startDate: { type: Types.Date, format: 'MMM Do YYYY', default: Date.now, required: true, initial: true },
-  endDate: { type: Types.Date, format: 'MMM Do YYYY', default: Date.now, required: true, initial: true }
+  endDate: { type: Types.Date, format: 'MMM Do YYYY', default: Date.now, required: true, initial: true },
+	leaders: { type: Types.Relationship, ref: 'User', many: true },
+	cost: { type: Types.Money, initial: true }
 });
 
 SummerMission.defaultColumns = 'name, location, startDate, endDate';
