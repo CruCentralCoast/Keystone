117d34e081f3db9f05570270b08667665f3d90e4
Andrew Kennedy <andrewk36@gmail.com>
2016-01-12 23:24:22 -0800

    added models for resources and tags

---
+++ models/ResourceTag.js
@@ -0,0 +1,23 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+/**
+ * ResourceTag Model
+ * ==========
+ */
+
+var ResourceTag = new keystone.List('ResourceTag', {
+	map: { name: 'title' },
+	autokey: { path: 'slug', from: 'title', unique: true },
+	singular: 'Resource Tag',
+	plural: 'Resource Tags'
+});
+
+ResourceTag.add({
+	title: { type: String, required: true, initial: true },
+	resources: { type: Types.Relationship, ref: 'Resource', many: true }
+});
+
+ResourceTag.relationship({ path: 'tagged', ref: 'Resource', refPath: 'tags' });
+
+ResourceTag.register();
