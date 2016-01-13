cd1b73e0b2d0b866f4cd1b86ccabb54be0428b0d
Andrew Kennedy <andrewk36@gmail.com>
2016-01-13 01:05:57 -0800

    more config changes

--- models/Resource.js
+++ models/Resource.js
@@ -17,7 +17,8 @@ Resource.add({
 	url: { type: Types.Url, required: true, initial: true },
 	type: { type: Types.Select, emptyOption: false, initial: true, required: true, numeric: false, options: ['article', 'audio', 'video'] },
 	date: { type: Types.Date, yearRange: [2000, 2030] },
-	title: { type: Types.Text, initial: true, required: true}
+	title: { type: Types.Text, initial: true, required: true},
+	tags: { type: Types.Relationship, initial: true, required: false, ref: 'ResourceTag', many: true }
 });
 
 Resource.relationship({ path: 'tags', ref: 'ResourceTag', refPath: 'title' });
