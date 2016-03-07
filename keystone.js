7a3e34d2af7426fef960117e658f2a921d5d91cf
Andrew Kennedy <andrewk36@gmail.com>
2016-03-07 04:53:33 -0800

    more nav

--- keystone.js
+++ keystone.js
@@ -97,7 +97,7 @@ keystone.set('email tests', require('./routes/emails'));
 
 keystone.set('nav', {
   'campuses': 'campus',
-//  'community-groups': 'community-groups'
+  'community-groups': 'community-groups',
 	'events': 'events',
 	'users': 'users',
 	'ministries': 'ministries',
