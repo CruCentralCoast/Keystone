50dadd62ef9f2f68aa6672fb0cf950c3f4ed00cc
Ubuntu <ubuntu@ip-172-31-29-10.us-west-2.compute.internal>
2016-03-09 19:17:42 +0000

    resources now use restricted field rather than leader tag to filter

--- routes/api/resource.js
+++ routes/api/resource.js
@@ -12,20 +12,13 @@ var model = Resource.model;
 
 var properties = propertyReader(root + '/properties.ini');
 var leaderAPIKey = properties.path().leader.api.key;
-var leaderTagID = properties.path().leader.tag.id;
 
-Array.prototype.contains = function ( needle ) {
-   for (i in this) {
-       if (this[i] == needle) return true;
-   }
-   return false;
-}
 
 router.route('/list')
 	.get(function(req, res) {
 		var params = {};
 		if (req.query.LeaderAPIKey != leaderAPIKey) {
-			params = {"tags": { "$nin": [leaderTagID]}};
+			params = {"restricted": false};
 		}
 		model.find(params).exec(function(err, items) {
 			if (err) return res.apiError('database error', err);
@@ -39,8 +32,7 @@ router.route('/:id')
 		model.findById(req.params.id).exec(function(err, item) {
 			if (err) return res.apiError('database error', err);
 			if (!item) return res.apiError('not found');
-			if (item.tags.contains(leaderTagID)
-				&& req.query.LeaderAPIKey != leaderAPIKey) {
+			if (item.restricted && req.query.LeaderAPIKey != leaderAPIKey) {
 				return res.apiError("not authorized");
 			}
 			res.json(item);
@@ -50,7 +42,7 @@ router.route('/:id')
 router.route('/find')
 	.post(function(req, res) {
 		if (req.query.LeaderAPIKey != leaderAPIKey) {
-			req.body = {"$and":[req.body, {"tags": { "$nin": [leaderTagID] }}]}
+			req.body = {"$and":[req.body, {"restricted": false}]}
 		}
 		restUtils.find(model, req, res);
 	});
@@ -58,7 +50,7 @@ router.route('/find')
 router.route('/search')
 	.post(function(req, res) {
 		if (req.query.LeaderAPIKey != leaderAPIKey) {
-			req.body.conditions = {"$and":[req.body.conditions, {"tags": { "$nin": [leaderTagID] }}]}
+			req.body.conditions = {"$and":[req.body.conditions, {"restricted":false}]}
 		}
 		restUtils.search(model, req, res);
 	});
