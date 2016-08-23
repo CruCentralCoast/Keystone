3b786f1d330b7c6bdc5d33162b1528f4ff309e6a
Erica Solum <erica.solum@yahoo.com>
2016-08-23 12:20:35 -0700

    Fixed indentations and spacing.

--- routes/api/authUtils.js
+++ routes/api/authUtils.js
@@ -10,32 +10,32 @@ module.exports = {
   
 		if (!req.body.username || !req.body.password) return res.json({ success: false });
   
-			keystone.list('User').model.findOne({ email: req.body.username }).exec(function(err, user) {
-				if (err || !user) {
-					return res.json({
-						success: false,
+		keystone.list('User').model.findOne({ email: req.body.username }).exec(function(err, user) {
+			if (err || !user) {
+				return res.json({
+					success: false,
 						message: (err && err.message ? err.message : false) || 'Sorry, there was an issue signing you in, please try again.'
-					});
-				}
+				});
+			}
 
-				keystone.session.signin({ email: user.email, password: req.body.password }, req, res, function(user) {
-                    if (req.body.gcmId) {
-                        var model = keystone.list("User").model;
-                        user.getUpdateHandler(req).process({email:user.email, gcmId:req.body.gcmId}, function(err) {
-                            console.log(err)
-                        })
-                    }
-					return res.json({
-						success: true,
-						LeaderAPIKey: leaderAPIKey 
-					});
-				}, function(err) {
-					return res.json({
-						success: false,
-						message: (err && err.message ? err.message : false) || 'Sorry, there was an issue signing you in, please try again.'
-					});
+			keystone.session.signin({ email: user.email, password: req.body.password }, req, res, function(user) {
+                if (req.body.gcmId) {
+                    var model = keystone.list("User").model;
+                    user.getUpdateHandler(req).process({email:user.email, gcmId:req.body.gcmId}, function(err) {
+                        console.log(err)
+                    })
+                }
+				return res.json({
+					success: true,
+					LeaderAPIKey: leaderAPIKey 
+				});
+			}, function(err) {
+				return res.json({
+					success: false,
+					message: (err && err.message ? err.message : false) || 'Sorry, there was an issue signing you in, please try again.'
 				});
 			});
+		});
 	},
 
 	signout: function(req, res) {
