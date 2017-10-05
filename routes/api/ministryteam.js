16230797f4ae4d742ce3c5f3ba85168b8eb3bb7c
fedorareis <fedorareis@gmail.com>
2017-10-04 18:46:09 -0700

    Linting, formatting, and Device Type fixes

--- routes/api/ministryteam.js
+++ routes/api/ministryteam.js
@@ -1,5 +1,4 @@
-var async = require('async'),
-    keystone = require('keystone'),
+var keystone = require('keystone'),
     express = require('express'),
     router = express.Router(),
     restUtils = require('./restUtils');
@@ -11,41 +10,41 @@ var notifications = require('./notificationUtils');
 var fcmUtils = require('./fcmUtils');
 
 router.route('/')
-	.get(function(req, res, next) {
-		model.find().populate('leaders').exec(function(err, teams) {
+    .get(function (req, res) {
+        model.find().populate('leaders').exec(function (err, teams) {
             if (err) return res.status(400).send(err);
             return res.json(teams);
         });
-	})
-	.post(function(req, res, next) {
-		restUtils.create(model, req, res);
-	});
+    })
+    .post(function (req, res) {
+        restUtils.create(model, req, res);
+    });
 
 router.route('/:id')
-	.get(function(req, res, next) {
-		model.findById(req.params.id).populate('leaders').exec(function(err, team) {
+    .get(function (req, res) {
+        model.findById(req.params.id).populate('leaders').exec(function (err, team) {
             if (err) return res.status(400).send(err);
             return res.json(team);
         });
-	})
-	.patch(function(req, res, next) {
-		restUtils.update(model, req, res);
-	});
+    })
+    .patch(function (req, res) {
+        restUtils.update(model, req, res);
+    });
 
 router.route('/search')
-	.post(function(req, res, next) {
-		restUtils.search(model, req, res);
-	});
+    .post(function (req, res) {
+        restUtils.search(model, req, res);
+    });
 
 router.route('/enumValues/:key')
-	.get(function(req, res, next) {
-		restUtils.enumValues(model, req, res);
-	});
+    .get(function (req, res) {
+        restUtils.enumValues(model, req, res);
+    });
 
 router.route('/find')
-	.post(function(req, res, next) {
-		var data = (req.method == 'POST') ? req.body : req.query;
-        
+    .post(function (req, res) {
+        var data = (req.method == 'POST') ? req.body : req.query;
+
         // default 0 means no limit
         var lim = req.query.limit ? req.query.limit : 0;
         // gets order by dynically creating the object specified
@@ -63,20 +62,20 @@ router.route('/find')
             }
         }
 
-        model.find(data, selects).limit(lim).sort(order).populate('leaders').exec(function(err, items) {
+        model.find(data, selects).limit(lim).sort(order).populate('leaders').exec(function (err, items) {
             if (err) return res.send(err);
             if (!items) return res.send('not found');
 
             return res.json(items);
         });
-	});
+    });
 
-router.route('/:id/join').post(function(req, res, next) {
+router.route('/:id/join').post(function (req, res) {
     var ministryTeamId = req.params.id;
     var name = req.body.name;
     var phone = req.body.phone;
 
-    model.findById(ministryTeamId).populate("leaders").exec(function(err, team) {
+    model.findById(ministryTeamId).populate("leaders").exec(function (err, team) {
         if (err) return res.apiError('failed to join ministry team', err);
 
         var leaderInfo = [];
@@ -84,23 +83,26 @@ router.route('/:id/join').post(function(req, res, next) {
 
         // for every leader send them the person that joined's info
         // and get their info to send to the user
-        team.leaders.forEach(function(leader) {
+        team.leaders.forEach(function (leader) {
             leaderInfo.push({
                 name: leader.name,
                 phone: leader.phone,
                 email: leader.email
             });
             if (leader.fcmId) {
-                fcmTokens.push(leader.fcmId);
+                fcmTokens.push({
+                    id: leader.fcmId,
+                    device: leader.deviceType
+                });
             }
         });
 
-        var payload = fcmUtils.createMessage(team.name,
-            name + " wants to join " + team.name + ". Their phone number is " + phone + ".");
+        var message = name + " wants to join " + team.name + ". Their phone number is " + phone + ".";
 
-        fcmTokens.forEach(function(token) {
+        fcmTokens.forEach(function (token) {
             if (token) {
-                notifications.sendToDevice(token, payload, function(err, response, body) {
+                var payload = fcmUtils.createMessage(team.name, message, token.device);
+                notifications.sendToDevice(token.id, payload, function (err, response, body) {
                     console.log(body);
                 });
             }
@@ -114,8 +116,8 @@ router.route('/:id/join').post(function(req, res, next) {
 });
 
 router.route("/:id/leaders")
-    .get(function(req, res, next) {
-        model.findOne({_id: req.params.id}).populate("leaders").exec(function(err, team) {
+    .get(function (req, res) {
+        model.findOne({ _id: req.params.id }).populate("leaders").exec(function (err, team) {
             if (err) return res.status(400).send(err);
             console.log(team.leaders);
             return res.json(team.leaders);
