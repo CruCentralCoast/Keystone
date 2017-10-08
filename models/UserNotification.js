bc365823c1ce9d3775d2c0a6ee247928ed1c50fa
fedorareis <fedorareis@gmail.com>
2017-10-07 18:47:41 -0700

    Added User Notifications

--- models/UserNotification.js
+++ models/UserNotification.js
@@ -6,17 +6,18 @@ var Types = keystone.Field.Types;
  * =============
  */
 
-var Notification = new keystone.List('UserNotification', {
+var UserNotification = new keystone.List('UserNotification', {
     defaultSort: "time"
 });
 
-Notification.add({
-    title: { type: String, required: true, initial: false},
-    message: { type: String, required: true, initial: true },
+UserNotification.add({
+    title: { type: String, required: false, initial: false},
+    body: { type: String, required: true, initial: false },
+    subTitle: { type: String, required: false, initial: false },
     time: { type: Types.Datetime, required: true, initial: true, default: Date.now },
     sent: { type: Types.Boolean, initial: true, default: false },
-    user: { type: Types.Relationship, initial: true, ref: 'User', label: 'Ministries to send to (none = everyone)' }
+    user: { type: Types.Relationship, initial: true, ref: 'User'}
 });
 
-Notification.defaultColumns = 'message, sent, time, ministries';
-Notification.register();
+UserNotification.defaultColumns = 'message, sent, time, user';
+UserNotification.register();
